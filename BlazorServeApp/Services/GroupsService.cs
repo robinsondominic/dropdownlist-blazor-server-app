86c63f0da3120556072472997bdd946b5f074f7c
robinsondominic <robinsondominic@outlook.com>
2022-09-03 17:43:13 +0100

    commit
    another commit

---
+++ BlazorServeApp/Services/GroupsService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorServeApp.Data;
+using BlazorServeApp.Models;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace BlazorServeApp.Services
+{
+
+    public class GroupsService
+    {
+        protected readonly ApplicationDbContext _dbcontext;
+
+        public GroupsService(ApplicationDbContext _db) 
+        {
+            _dbcontext = _db;
+        }
+
+        public List<GroupsClass> groupsobj()
+        {
+            return _dbcontext.Groups.ToList();
+        }
+
+        public List<UserClass> usersobj()
+        {
+            return _dbcontext.User.ToList();
+        }
+    }
+}
---
+++ BlazorServeApp/Services/UserService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorServeApp.Data;
+using BlazorServeApp.Models;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace BlazorServeApp.Services
+{
+
+    public class UserService
+    {
+        protected readonly ApplicationDbContext _dbcontext;
+
+        public UserService(ApplicationDbContext _db) 
+        {
+            _dbcontext = _db;
+        }
+
+        public List<UserClass> userobj()
+        {
+            return _dbcontext.User.ToList();
+        }
+    }
+}
