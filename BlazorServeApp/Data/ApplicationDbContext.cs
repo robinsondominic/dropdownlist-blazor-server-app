742dd2080fe37d09440e7e6b1fc932a365a4fdfd
robinsondominic <robinsondominic@outlook.com>
2022-09-03 16:30:50 +0100

    main commit
    main commit

---
+++ BlazorServeApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using BlazorServeApp.Models;
+
+namespace BlazorServeApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+
+        public DbSet<GroupsClass> Groups { get; set; }
+        public DbSet<UserClass> User { get; set; }
+    }
+}
